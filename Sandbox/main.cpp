8cdcb7f4e6aa99c6e4e1182362369f8d69d8111f
AmongTheBricks <68630422+AmongTheBricks@users.noreply.github.com>
2024-11-05 20:09:31 -0500

    Sandbox test

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,5 +1,11 @@
+#include "core/logging/Logger.h"
+#include "core/logging/LoggerFactory.h"
 
 int main()
 {
+    hive::Logger* logger = hive::LoggerFactory::createLogger(hive::LogOutputType::Console, hive::LogLevel::Debug);
+    hive::Logger::init(logger);
+    hive::Logger::log("Hi", hive::LogLevel::Error);
 
+    return 0;
 }
